7adbea710d0279cf7f987a6a0b9409bdc7847d99
EzequielTecsid <ezequiel@tecsid.com>
2024-12-30 08:51:46 -0300

    Documentacion con Swagger

--- src/router.ts
+++ src/router.ts
@@ -11,9 +11,79 @@ import {
 import { handleInputErrors } from "./middleware";
 
 const router = Router();
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Product:
+ *      type: object
+ *      properties:
+ *        id:
+ *          type: integer
+ *          description: The Product ID
+ *          example: 1
+ *        name:
+ *          type: string
+ *          description: The product name
+ *          example: Monitor Curvo de 49 Pulgadas
+ *        price:
+ *          type: number
+ *          description: The product price
+ *          example: 300
+ *        availability:
+ *          type: boolean
+ *          description: The product availability
+ *          example: true
+ */
 
-// Routing
+/**
+ * @swagger
+ * /api/products:
+ *    get:
+ *      summary: Get a list of products
+ *      tags:
+ *        - Products
+ *      description: Return a list of products
+ *      responses:
+ *        200:
+ *          description: Successful response
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: array
+ *                items:
+ *                  $ref: '#/components/schemas/Product'
+ * 
+ */
 router.get("/", getProducts);
+
+/**
+ * @swagger
+ * /api/products/{id}:
+ *  get:
+ *    summary: Get a product by ID
+ *    tags:
+ *      - Products
+ *    description: Return a product based on its unique ID
+ *    parameters:
+ *    - in: path
+ *      name: id
+ *      description: The ID of the product to retrieve
+ *      required: true
+ *      schema:
+ *        type: integer
+ *    responses:
+ *      200:
+ *        description: Successful Response
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Product'
+ *      404:
+ *        description: Not Found
+ *      400:
+ *        description: Bad Request - Invalid ID
+ */
 router.get(
   "/:id",
   param("id").isInt().withMessage("ID no válido"),
@@ -21,6 +91,39 @@ router.get(
   getProductById
 );
 
+
+/**
+ * @swagger
+ * /api/products:
+ *  post:
+ *    summary: Creates a new product
+ *    tags:
+ *      - Products
+ *    description: Returns a new record in the database
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              name:
+ *                type: string
+ *                example: "Monitor Curvo 49 Pulgadas"
+ *              price:
+ *                type: number
+ *                example: 300
+ *    responses:
+ *      201:
+ *        description: Product created succesfully
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Product'
+ *      400:
+ *        description: Bad Request - Invalid Input Data
+ * 
+ */
 router.post(
   "/",
   // Validacion
@@ -39,6 +142,51 @@ router.post(
   createProduct
 );
 
+
+/**
+ * @swagger
+ * /api/products/{id}:
+ *  put:
+ *    summary: Updates a product with user input
+ *    tags:
+ *      - Products
+ *    description: Returns the updated product
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        description: The ID of the product to retrieve
+ *        required: true
+ *        schema:
+ *          type: integer
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              name:
+ *                type: string
+ *                example: "Monitor Curvo 49 Pulgadas"
+ *              price:
+ *                type: number
+ *                example: 300
+ *              availability:
+ *                type: boolean
+ *                example: true
+ *    responses:
+ *      200:
+ *        description: Successul response
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Product'
+ *      400:
+ *        description: Bad Request - Invalid ID o Invalid Input Data
+ *      404:
+ *        description: Product Not Found
+ * 
+ */
 // PUT: Actualización y/o reemplazo de los datos del elemento completamente
 router.put(
   "/:id",
@@ -59,6 +207,33 @@ router.put(
   updateProduct
 );
 
+/**
+ * @swagger
+ * /api/products/{id}:
+ *  patch:
+ *    summary: Update Product availability
+ *    tags:
+ *      - Products
+ *    description: Returns the updated availability
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        description: The ID of the product to retrieve
+ *        required: true
+ *        schema:
+ *          type: integer
+ *    responses:
+ *      200:
+ *        description: Succesful response
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Product'
+ *      400:
+ *        description: Bad Request - Invalid ID
+ *      404:
+ *        description: Product Not Found
+ */
 //PATCH: Actualización de datos puntuales
 router.patch(
   "/:id",
@@ -68,6 +243,35 @@ router.patch(
   updateAvailability
 );
 
+/**
+ * @swagger
+ * /api/products/{id}:
+ *  delete:
+ *    summary: Deletes a product from database
+ *    tags:
+ *      - Products
+ *    description: Deletes a product from the database by ID
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        description: The ID of the product to remove
+ *        required: true
+ *        schema:
+ *          type: integer
+ *    responses:
+ *      200:
+ *        description: Succesful response
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: string
+ *              value: 'Producto eliminado'
+ *      400:
+ *        description: Bad Request - Invalid ID
+ *      404:
+ *        description: Product Not Found
+ *        
+ */
 router.delete('/:id', 
     param('id').isInt().withMessage('ID no válido'),
     handleInputErrors,
